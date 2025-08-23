61c237c2fa1a4cce947c6880937beb380c0c6639
kabirrifai03 <achmadkabir003@email.com>
2025-08-23 18:18:21 +0700

    update

--- api/index.php
+++ api/index.php
@@ -4,6 +4,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
+
+
 // Untuk preflight request OPTIONS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
