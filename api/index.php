97dde8767fca894ffd359ec9c63cefacc81bff22
kabirrifai03 <achmadkabir003@email.com>
2025-08-25 21:47:16 +0700

    update

--- api/index.php
+++ api/index.php
@@ -4,8 +4,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-DATABASE_URL = "postgresql://postgres:KmHgAwpftjhUqqfutZJanIWKsQbpAYIN@postgres.railway.internal:5432/railway"
-
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
